9019cc07667f0bec69121142347e4acd2ced5569
isiri <isiri@isiri-desktop.(none)>
2011-01-21 10:03:25 +0530

    config folder added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Affiliate::Application.config.secret_token = 'de7fa6f001ac4347c94582e2888c44e9b1bc3f54fb1adce06a9dbe80a4cfaec30e2c40961f075f9a12cde8af1140a737c734bcf41774a59ebc0fafc75b18efe1'
